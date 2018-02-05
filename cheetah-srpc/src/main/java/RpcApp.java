a437a2469d73e8420bd776aed35cc6845d2cca04
Ruan-Xin <415131215@qq.com>
2018-02-05 15:07:31 +0800

    add new mod

--- cheetah-srpc/src/main/java/RpcApp.java
+++ cheetah-srpc/src/main/java/RpcApp.java
@@ -3,7 +3,7 @@
  * @create 2018-02-05
  * @desc test
  */
-public class App {
+public class RpcApp {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
--- cheetah-support/src/main/java/SupportApp.java
+++ cheetah-support/src/main/java/SupportApp.java
@@ -3,7 +3,7 @@
  * @create 2018-02-05
  * @desc
  */
-public class App {
+public class SupportApp {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
