1edd2026ac4e43498cfa33a4d7daf1af9fc77026
Ruan-Xin <415131215@qq.com>
2018-02-05 14:57:03 +0800

    init

---
+++ cheetah-rpc/src/main/java/App.java
@@ -0,0 +1,10 @@
+/**
+ * @author ruanxin
+ * @create 2018-02-05
+ * @desc
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
