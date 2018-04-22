9cbecc567c36bfcb91734dde6cfb2cd665f2221e
Ruan-Xin <415131215@qq.com>
2018-04-22 15:51:28 +0800

    add cluster module

---
+++ cheetah-cluster/src/main/java/ClusterApp.java
@@ -0,0 +1,10 @@
+/**
+ * @author ruanxin
+ * @create 2018-04-22
+ * @desc
+ */
+public class ClusterApp {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
