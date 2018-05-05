961329cb0b1293ebe318f3f143862d22d49934ed
Ruan-Xin <415131215@qq.com>
2018-05-05 18:26:15 +0800

    new severnode start

---
+++ cheetah-demo/src/main/java/raft/NewRaftServerNodeStartDemo.java
@@ -0,0 +1,13 @@
+package raft;
+
+/**
+ * @author ruanxin
+ * @create 2018-05-05
+ * @desc
+ */
+public class NewRaftServerNodeStartDemo {
+
+    public static void main(String[] args) {
+        
+    }
+}
