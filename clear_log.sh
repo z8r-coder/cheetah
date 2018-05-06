505b59c4a1094d9a6c63102b24a05453d57ec967
Ruan-Xin <415131215@qq.com>
2018-05-06 20:46:41 +0800

    clear_log update

--- clear_log.sh
+++ clear_log.sh
@@ -2,3 +2,15 @@
 
 rm -rf raft/raft_log/*
 rm -rf raft/raft_meta/*
+
+rm -rf raft/nodeA/raft_log/*
+rm -rf raft/nodeA/raft_meta/*
+
+rm -rf raft/nodeB/raft_log/*
+rm -rf raft/nodeB/raft_meta/*
+
+rm -rf raft/nodeC/raft_log/*
+rm -rf raft/nodeC/raft_meta/*
+
+rm -rf raft/nodeD/raft_log/*
+rm -rf raft/nodeD/raft_meta/*
